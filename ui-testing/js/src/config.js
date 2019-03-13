61aebc2b832fce3803913e4695a5fb5a7d26fc96
Thomas Hunter <th@thomashunter.co.uk>
2019-03-13 15:27:08 +0000

    Moved JS test folder location for kotlin example

--- ui-testing/js/src/config.js
+++ ui-testing/js/src/config.js
@@ -7,7 +7,7 @@ export let wdCapabilities = {
   "deviceName": "Pixel 2 XL API 27",
     "platformName": "Android",
     "platformVersion": "8.1",
-    "app": resolve('../app/build/outputs/apk/byod/debug/the-hub_V1.0.0-DEBUG_byodDebug.apk') // Allows for a absolute path which can then be translated to a relative one.
+    "app": resolve('../../app/build/outputs/apk/byod/debug/the-hub_V1.0.0-DEBUG_byodDebug.apk') // Allows for a absolute path which can then be translated to a relative one.
 };
 
 
