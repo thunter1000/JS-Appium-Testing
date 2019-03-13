8d6a976e7bf41ec4978d1a47f5cd9f9e234c2f9c
Thomas Hunter <th@thomashunter.co.uk>
2019-03-13 09:51:28 +0000

    Created simple test that tests the test button

--- ui-testing/test/sample.test.js
+++ ui-testing/test/sample.test.js
@@ -1,19 +1,29 @@
-
 import { describe, it } from 'mocha';
+import { expect } from 'chai';
 import { getDriver } from '../src/providers/webdriver';
 
 const assert = require('assert');
 
 describe('Test the test button', () => {
-  let driver = getDriver({});
+  let driver;
 
-  it('Clicking the button should toggle visibility', async () => {
-    let testButton = await (await driver).elementByAccessibilityId('MainActivity-TestButton');
+  beforeEach(async () => driver = await getDriver({}));
 
-    await testButton.click();
+  afterEach(async () => await driver.quit());
 
-    let testText = await (await driver).elementByAccessibilityId('MainActivity-TestText'); // TODO throws exception not null when it cannot find the element.
+  it('Test text should not be displayed', async () => {
+    expect(await driver.elementByAccessibilityIdOrNull('MainActivity-TestText')).to.be.null;
+  });
 
-    assert.notEqual(testText, null, 'Test text should be visible');
+  it('Test text should be displayed after button click', async () => {
+    await driver.elementByAccessibilityId('MainActivity-TestButton').click();
+
+    expect(await driver.elementByAccessibilityIdOrNull('MainActivity-TestText')).to.not.be.null;
+  });
+
+  it('Test text should not be displayed after toggling the test button twice', async () => {
+    await driver.elementByAccessibilityId('MainActivity-TestButton').click().click();
+
+    expect(await driver.elementByAccessibilityIdOrNull('MainActivity-TestText')).to.be.null
   });
 })
