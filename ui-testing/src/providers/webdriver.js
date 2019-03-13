260915b542442dc7a3005e8b82c461b555851a97
Thomas Hunter <th@thomashunter.co.uk>
2019-03-13 09:51:28 +0000

    Created simple test that tests the test button

--- ui-testing/src/providers/webdriver.js
+++ ui-testing/src/providers/webdriver.js
@@ -1,9 +1,9 @@
+import { promiseChainRemote } from 'wd';
 import { wdConnection, wdCapabilities } from '../config';
 
-const wd = require('wd');
-
 export async function getDriver({connection = wdConnection, capabilities = wdCapabilities}) {
-  let driver = wd.promiseChainRemote(connection);
+  let driver;
+  driver = promiseChainRemote(connection);
   await driver.init(capabilities);
   return driver;
 };
